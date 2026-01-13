713de901d65384a8142406aedc6f439382a42767
henryhwang007 <henryhwang007@gmail.com>
2026-01-13 18:18:08 +0900

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -11,7 +11,7 @@ def gradient_f(x: float, y: float) -> tuple:
     Returns:
         - A tuple (∂f/∂x, ∂f/∂y) for the given values of x and y.
     '''
-    return (2x + 2, 2y + 4)
+    return (2*x + 2, 2*y + 4)
     pass
 
 def gradient_descent(starting_values: tuple, learning_rate: float, num_iterations: int) -> tuple:
